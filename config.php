626ddf2d5406386f4ae53c56838529b527e18f77
darkdragon630 <71604929+darkdragon630@users.noreply.github.com>
2025-08-31 21:12:02 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
-$host = 'localhost';
-$db = 'saaz';
-$user = 'root';
-$pass = '';
+$host = 'db.fr-pari1.bengt.wasmernet.com';
+$db = 'dbNANgVXJBJEnGrupi8tAkXpz';
+$user = '4569b4f873728000025f9d30c379';
+$pass = '068b4569-b4f8-763b-8000-2f1c3f10046c';
+$port = '10272';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;dbname=$db;port=$port;charset=$charset";
 
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
@@ -14,7 +15,7 @@ $options = [
 ];
 
 try {
-    $koneksi = new PDO($dsn, $user, $pass, $options);
+    $koneksi = new PDO($dsn, $user, $pass, $port, $options);
     $koneksi->exec("SET NAMES 'utf8'");
 } catch (PDOException $e) {
     die('koneksi gagal: ' . $e->getMessage());
