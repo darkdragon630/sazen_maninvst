c9380c037e049ee84fbb61ebc02d548e893d612a
darkdragon630 <71604929+darkdragon630@users.noreply.github.com>
2025-09-02 21:39:02 +0700

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -108,6 +108,7 @@ if (isset($_POST['logout'])) {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 </head>
 <body>
     <!-- Loading Screen -->
