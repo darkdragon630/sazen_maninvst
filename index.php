405451ce02608d4df69cc5aa3a115a79735d63b8
darkdragon630 <71604929+darkdragon630@users.noreply.github.com>
2025-09-06 03:56:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -544,7 +544,7 @@ $performance_ytd = $total_investasi > 0 ? ($total_keuntungan / $total_investasi)
 
          document.querySelector("footer").addEventListener("dblclick", function() {
              alert("🎉 Selamat! Anda menemukan pesan tersembunyi.");
-             window.location.href = "pesan_tersembunyi.png"; 
+             window.location.href = "pesan.html"; 
         });
         // Intersection Observer for animations
         const observer = new IntersectionObserver((entries) => {
